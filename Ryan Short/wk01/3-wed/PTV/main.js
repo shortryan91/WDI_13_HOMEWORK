c6dd272171b31cc54b3981e096c01b405174fb6e
Ryan Short <rcshort9376@hotmail.com>
2017-09-02 14:19:34 +1000

    switching lines works

--- Ryan Short/wk01/3-wed/PTV/main.js
+++ Ryan Short/wk01/3-wed/PTV/main.js
@@ -6,28 +6,39 @@ var Sandringham = ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran','Winds
 var trainLines = [Alamein, GlenWaverly, Sandringham];
 
 var interchange = 'Richmond';
+var firstLine = [];
+var secondLine = [];
 
 
 
 // find the origin station
+
 var origin = prompt('pick your current station');
-for(var i = 0; i < trainLines.length; i++) {
-  if(trainLines[i].includes(origin)) {
-    var startLine = trainLines[i];
-    console.log('your current location is: ' + origin)
+if (origin === interchange) {
+  console.log("your current location is: " + origin)
+} else if (origin !== interchange ) {
+  for(var i = 0; i < trainLines.length; i++) {
+    if(trainLines[i].includes(origin)) {
+      var startLine = trainLines[i];
+      console.log('your current location is: ' + origin)
     }
+  }
 }
 
 
 // // find the destination
 var destination = prompt('pick where you would like to go?');
-for( var i = 0; i < trainLines.length; i++) {
-  if (trainLines[i].includes(destination)) {
-    var endLine = trainLines[i];
-    console.log('Your destination is: ' + destination)
-
-  }
+if (destination === interchange) {
+  console.log("your current destination is: " + destination);
+} else if (destination !== interchange) {
+    for( var i = 0; i < trainLines.length; i++) {
+      if (trainLines[i].includes(destination)) {
+        var endLine = trainLines[i];
+        console.log('Your destination is: ' + destination)
+      }
+    }
 }
+
 
 // check start and destination on the same line
 if (startLine === endLine) {
@@ -36,19 +47,38 @@ if (startLine === endLine) {
   console.log('you need to switch at Richmond');
 }
 
-// switching lines
-// if(startLine !== endLine) {
-//   startLine = startLine.slice(origin , 'Richmond'+1);
-// }
-
-
-
 
 // print out a jounrey
 //need to find the right Richmond
-var indexStart = startLine.indexOf(origin);
-var indexEnd = endLine.indexOf(destination);
+// checking if you are starting at richmond or not
+if (origin === interchange) {
+  var indexStart = endLine.indexOf(origin);
+} else {
+  var indexStart = startLine.indexOf(origin);
+}
+// checking if you end at richmond or not
+if (destination === interchange){
+  var indexEnd = startLine.indexOf(destination)
+} else {
+  var indexEnd = endLine.indexOf(destination);
+}
 var indexes = [indexStart, indexEnd];
+
+// switching lines
+
+// loop through the array from the origin to richmond station and stop
+if(startLine !== endLine) {
+  for(var index = 0; index < startLine.length; index++) {
+    firstLine = startLine.slice(indexStart, startLine.indexOf(interchange)+1)
+  }
+// loop through from richmond to destination
+  for(var index = 0; index < endLine.length; index++) {
+    secondLine = endLine.slice(endLine.indexOf(interchange), indexEnd + 1)
+  }
+}
+
+
+
 
 // check if the index's are the right way
 
@@ -59,13 +89,23 @@ if(indexStart > indexEnd){
   var trip = startLine.slice(indexes[0], indexes[1] + 1);
 }
 
+
+
+var twoLineTrip = firstLine.pop();
+twoLineTrip = firstLine.concat(secondLine);
+
+if (startLine === endLine) {
 console.log(trip.join(" -----> "));
-
-
-
+}else {
+console.log(twoLineTrip.join(" -----> "))
+}
 //  the interchange problem
 
 
 
 // the total amount of stops
- console.log(trip.length -1 + " stops total")
+if(startLine !== endLine) {
+console.log(twoLineTrip.length-1 + " stops total");
+} else {
+ console.log(trip.length-1 + " stops total");
+}
