2986e31745683a63256af9f1617d43419b89832e
Ryan Short <rcshort9376@hotmail.com>
2017-09-22 15:41:37 +1000

    movies stuff

--- Ryan Short/wk04/4-thu/movies_one/app.rb
+++ Ryan Short/wk04/4-thu/movies_one/app.rb
@@ -6,6 +6,8 @@ require 'httparty'
 # api key 2f6435d9
 # http://www.omdbapi.com/?apikey=[2f6435d9]&
 
+comments = ['great movie']
+
 get '/' do
 
   erb :index
@@ -13,17 +15,29 @@ end
 
 
 get '/search_page' do
-  url = "http://www.omdbapi.com/?s="+ params[:title] + "&apikey=2f6435d9"
+  url = "http://www.omdbapi.com/?s="+ params[:title] + "&apikey=#{ENV['OMDB_API_KEY']}"
   response = HTTParty.get(url)
-  @search = response.parsed_response
-  # binding.pry
 
+  @search = response.parsed_response
+  if @search['totalResults'] == "1"
+    redirect "/profile?title=#{params[:title]}"
+  else
   erb :search_page
+ end
 end
 
 get '/profile' do
-  url = 'http://omdbapi.com/?t=' + params[:title] +  '&apikey=2f6435d9'
+  url = 'http://omdbapi.com/?t=' + params[:title] +  "&apikey=#{ENV['OMDB_API_KEY']}"
   response = HTTParty.get(url)
   @result = response.parsed_response
+  @comments = comments
   erb :profile
+end
+
+post '/leave_comment' do
+
+  @comments = comments
+  comments.push(params[:comment])
+
+  redirect "/profile?title=#{params[:title]}"
 end
